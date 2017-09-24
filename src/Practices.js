ad3d9556e994bb0c4b7f358dc5d5a67abe1dfcf1
Bart Wijnants <bartwijnants@outlook.com>
2017-09-24 12:12:38 +0200

    cleanup

--- src/Practices.js
+++ src/Practices.js
@@ -9,12 +9,28 @@ function Practices() {
     new Practice("PERFORMANCE", "is always on our mind", "flaticon-clock"),
     new Practice("HELP", "when needed", "flaticon-people-2"),
     new Practice("QUALITY", "be a good scout", "flaticon-cleaner"),
-    new Practice("DONT BLAME", "but give constructive feedback", "flaticon-people-1"),
+    new Practice(
+      "DONT BLAME",
+      "but give constructive feedback",
+      "flaticon-people-1"
+    ),
     new Practice("TEST", "all the things", "flaticon-flasks"),
-    new Practice("APPLY XP", "pairing, TDD, continuous everything", "flaticon-extreme-snowboard-silhouette"),
-    new Practice("KNOWLEDGE SHARING", "is important", "flaticon-home-delivery-service"),
+    new Practice(
+      "APPLY XP",
+      "pairing, TDD, continuous everything",
+      "flaticon-extreme-snowboard-silhouette"
+    ),
+    new Practice(
+      "KNOWLEDGE SHARING",
+      "is important",
+      "flaticon-home-delivery-service"
+    ),
     new Practice("DO", "what you are afraid to do", "flaticon-people"),
-    new Practice("FOLLOW UP", "on actions", "flaticon-compass-with-white-needles"),
+    new Practice(
+      "FOLLOW UP",
+      "on actions",
+      "flaticon-compass-with-white-needles"
+    )
   ].map(p => <PracticeComponent key={p.title} practice={p} />);
   return <div className="practices">{practices}</div>;
 }
